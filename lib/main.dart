e5f685f5a97bd031f2077f6ec3456105d5705470
hafidjs <afidzpratama@gmail.com>
2025-10-19 05:33:28 +0700

    GetX Get.create

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_getx/pages/home/home.dart';
 import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -10,55 +11,5 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(home: HomePage());
-  }
-}
-
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Home Page")),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Home Page"),
-            OutlinedButton(
-              onPressed: () => Get.to(() => CountPage()),
-              child: Text("Next Page >>"),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class CountPage extends StatelessWidget {
-  final count = 0.obs;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Count Page")),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Obx(() => Text("${count}", style: TextStyle(fontSize: 25))),
-            // OutlinedButton(onPressed: ()=> Get.to(() => CountPage()), child: Text("Next Page >>"))
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => Get.putAsync<SharedPreferences>(() async {
-          final prefs = await SharedPreferences.getInstance();
-          await prefs.setInt('counter', 99);
-
-          count.value = prefs.getInt('counter')!.toInt();
-          return prefs;
-        }),
-      ),
-    );
   }
 }
