bddbb48e268373b1dae5fef31ef30ed039af12dc
hafidjs <afidzpratama@gmail.com>
2025-10-19 17:10:31 +0700

    GetX Change Theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_getx/utils/translation.dart';
 import 'package:get/get.dart';
 
 void main() {
@@ -8,36 +7,16 @@ void main() {
 
 class MyApp extends StatelessWidget {
 
+
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      translations: MyTranslations(),
-      locale: Locale('en', 'US'),
-      title: 'Flutter Translation',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'title'.tr),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-
-  const MyHomePage({super.key, required this.title});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-        actions: [
-          IconButton(onPressed: () => Get.updateLocale(Locale('id')), icon: Icon(Icons.language)),
-        ],
-      ),
-      body: Center(
-        child: Text('body'.tr),
+      theme: ThemeData.dark(),
+      home: Scaffold(
+        appBar: AppBar(),
+        floatingActionButton: FloatingActionButton(onPressed: () {
+          Get.changeTheme(Get.isDarkMode ? ThemeData.light() : ThemeData.dark());
+        }),
       ),
     );
   }
